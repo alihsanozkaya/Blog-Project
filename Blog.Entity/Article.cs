dd547111eda582367d4b11e396e72704ce4fe041
Ali İhsan <ihsanozkaya1903@gmail.com>
2023-06-01 18:10:12 +0300

    Proje dosyası ekle.

---
+++ Blog.Entity/Article.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Core.Entities;
+
+namespace Blog.Entity.Entities
+{
+    public class Article : EntityBase, IEntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewCount { get; set; }
+        public int CategoryId { get; set; } //Guid
+        public Category Category { get; set; }
+        public int ImageId { get; set; } //Guid
+        public Image Image { get; set; }
+    }
+}
